b8e905a9a1c953c90db2c8022c9166483dd078b0
강예린 <yerin7786@gmail.com>
2024-11-20 21:05:26 +0900

    initial commit

---
+++ src/main/java/spring/univ_board/UnivBoardApplication.java
@@ -0,0 +1,13 @@
+package spring.univ_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/univ_board/UnivBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.univ_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
