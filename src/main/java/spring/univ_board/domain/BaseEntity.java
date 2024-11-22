67a29eec497e3623a61be21d26f56b8af771b7d9
강예린 <yerin7786@gmail.com>
2024-11-22 23:50:56 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/spring/univ_board/domain/BaseEntity.java
@@ -0,0 +1,25 @@
+package spring.univ_board.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+}
