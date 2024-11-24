d2811ddd21f38bb5ee616a061eddb4f8a5d840ad
강예린 <yerin7786@gmail.com>
2024-11-24 16:37:51 +0900

    feat: @EnableJpaAuditing 추가 BaseEntity의 @EntityListeners가 작동하기 위해 @EnableJpaAuditing 필요

--- src/main/java/spring/univ_board/UnivBoardApplication.java
+++ src/main/java/spring/univ_board/UnivBoardApplication.java
@@ -2,8 +2,10 @@ package spring.univ_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // BaseEntity의 @EntityListeners가 작동하기 위해서 @EnableJpaAuditing이 필요
 public class UnivBoardApplication {
 
 	public static void main(String[] args) {
