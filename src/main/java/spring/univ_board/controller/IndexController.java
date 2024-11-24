b6b626d5eaf7269e898c61afed688d4681f252e9
강예린 <yerin7786@gmail.com>
2024-11-24 20:07:24 +0900

    feat: @Slf4j 어노테이션 추가

--- src/main/java/spring/univ_board/controller/IndexController.java
+++ src/main/java/spring/univ_board/controller/IndexController.java
@@ -4,13 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 @Slf4j
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        log.info("BoardIndexController");
         return "index";
     }
 }
