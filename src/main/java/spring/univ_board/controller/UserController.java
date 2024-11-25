466df3e77893d5629541fd9f747f0e9bf2cbd004
강예린 <yerin7786@gmail.com>
2024-11-25 23:30:31 +0900

    refactor: login -> kakaoLogin 메서드명 변경 - 기본 login과 kakao login 구분

--- src/main/java/spring/univ_board/controller/UserController.java
+++ src/main/java/spring/univ_board/controller/UserController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import spring.univ_board.controller.dto.ResponseDto;
 import spring.univ_board.controller.dto.request.SignUpRequest;
 import spring.univ_board.controller.dto.response.SignUpResponse;
@@ -17,14 +14,15 @@ import spring.univ_board.service.UserService;
 
 @Slf4j
 @Controller
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final KakaoService kakaoService;
     private final UserService userService;
 
-    @GetMapping("/login")
-    public String login(Model model) { // 웹 페이지로 사용자를 리다이렉트하고, 해당 페이지에서는 Kakao 로그인 URL을 사용할 수 있도록 모델에 추가
+    @GetMapping("/login/kakao")
+    public String kakaoLogin(Model model) {
         model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
         return "user/login";
     }
