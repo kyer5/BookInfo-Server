b3460b3c39f26d2ec1d645c8d6ea6dcc844a8ec3
강예린 <yerin7786@gmail.com>
2024-11-26 20:29:42 +0900

    refactor: signup html 경로 변경

--- src/main/java/spring/univ_board/controller/UserController.java
+++ src/main/java/spring/univ_board/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
 
     @GetMapping("/signup")
     public String signup() {
-        return "signup";
+        return "user/signup";
     }
 
     @PostMapping("/signup")
