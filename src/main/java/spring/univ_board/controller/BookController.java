7ab09200169c2b10472375a6724fbc70d32e259a
강예린 <yerin7786@gmail.com>
2024-11-28 18:47:34 +0900

    refactor: 책 리스트 조회 HTTP 변경 - 조회 기능이므로 POST -> GET으로 변경

--- src/main/java/spring/univ_board/controller/BookController.java
+++ src/main/java/spring/univ_board/controller/BookController.java
@@ -1,32 +1,28 @@
 package spring.univ_board.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import spring.univ_board.controller.dto.ResponseDto;
+import spring.univ_board.controller.dto.response.BookDetailsResponse;
 import spring.univ_board.controller.dto.response.BookListResponse;
 import spring.univ_board.service.BookService;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
+@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
 
-    @GetMapping("/book/search")
-    public String search() {
-        return "naverBookList";
-    }
-
-    @PostMapping("/book/list")
-    public String bookList(@ModelAttribute("keyword") String keyword, Model model) throws Exception {
+    @GetMapping("/list")
+    public String bookList(@ModelAttribute("keyword") String keyword, Model model) throws IOException {
         List<BookListResponse> bookListResponse = bookService.getBookList(keyword);
         model.addAttribute("bookListResponse", bookListResponse);
-        return "naverBookList";
+        return "book/naverBookList";
     }
 
 }
