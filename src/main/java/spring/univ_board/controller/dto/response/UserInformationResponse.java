6db18a4df848ca73b8b3f4553ad07d0155fa0c42
강예린 <yerin7786@gmail.com>
2024-12-17 23:50:36 +0900

    feat: accountType 필드 추가 - 회원 정보 수정 시 회원 타입 구분 - 카카오 회원일 시 비밀번호, 닉네임 변경 불가능하도록 제한하기 위함

--- src/main/java/spring/univ_board/controller/dto/response/UserInformationResponse.java
+++ src/main/java/spring/univ_board/controller/dto/response/UserInformationResponse.java
@@ -2,6 +2,7 @@ package spring.univ_board.controller.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import spring.univ_board.domain.value.AccountType;
 
 @Getter
 public class UserInformationResponse {
@@ -9,11 +10,14 @@ public class UserInformationResponse {
     private String email;
     private String nickname;
     private String phone;
+    private AccountType accountType;
 
     @Builder
-    public UserInformationResponse(String email, String nickname, String phone) {
+
+    public UserInformationResponse(String email, String nickname, String phone, AccountType accountType) {
         this.email = email;
         this.nickname = nickname;
         this.phone = phone;
+        this.accountType = accountType;
     }
 }
