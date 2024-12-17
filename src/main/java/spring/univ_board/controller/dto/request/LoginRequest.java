b1f2d197baeec97d02b87987d97167f901d42176
강예린 <yerin7786@gmail.com>
2024-12-17 14:00:29 +0900

    refactor: 불필요한 생성자 삭제

--- src/main/java/spring/univ_board/controller/dto/request/LoginRequest.java
+++ src/main/java/spring/univ_board/controller/dto/request/LoginRequest.java
@@ -11,12 +11,4 @@ public class LoginRequest {
 
     @NotBlank(message = "비밀번호는 비어있을 수 없습니다.")
     private String password;
-
-    protected LoginRequest() {
-    }
-
-    public LoginRequest(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 }
